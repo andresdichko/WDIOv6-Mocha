db555783bf76376d327ae98467ae7b560cb3bfb0
andresdichko <andresdichko@users.noreply.github.com>
2021-01-08 14:42:29 -0300

    Update test.js

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,7 +2,7 @@ const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+    it.only('should login with valid credentials', () => {
         LoginPage.open();
 
         LoginPage.login('tomsmith', 'SuperSecretPassword!');
@@ -13,7 +13,7 @@ describe('My Login application', () => {
     });    it('should login with valid credentials2', () => {
         LoginPage.open();
 
-        
+
         LoginPage.login('tomsmith', 'SuperSecretPassword!');
         expect(SecurePage.flashAlert).toBeExisting();
         expect(SecurePage.flashAlert).toHaveTextContaining(
